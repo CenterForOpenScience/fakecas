6c3d867c9b27dc45b778402b83e0683edcb1a3c5
Chris Seto <chriskseto@gmail.com>
2016-04-05 12:49:36 -0400

    Update to echo v2 and check if users are registered or not

---
+++ static.go
@@ -0,0 +1,49 @@
+package main
+
+var UNREGISTERED = `<html lang="en" class=" js no-mobile desktop no-ie chrome chrome49 root-section gradient rgba opacity textshadow multiplebgs boxshadow borderimage borderradius cssreflections csstransforms csstransitions no-touch no-retina fontface domloaded w-2307 gt-240 gt-320 gt-480 gt-640 gt-768 gt-800 gt-1024 gt-1280 gt-1440 gt-1680 gt-1920 no-portrait landscape" id="login-page"><head>
+  <meta charset="UTF-8">
+
+  <title>Open Science Framework | Sign In</title>
+
+	<link rel="stylesheet" href="//staging-accounts.osf.io/css/cas.css">
+  <link rel="icon" href="//staging-accounts.osf.io/favicon.ico" type="image/x-icon">
+
+  <!--[if lt IE 9]>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.1/html5shiv.js" type="text/javascript"></script>
+  <![endif]--><script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script><script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script><style type="text/css">@media print{.lpiframeoverlay{display:none}}</style></head>
+<body id="cas"><div id="lptopspacer80974089" style="height: 40px;"></div>
+  <div id="container">
+      <header>
+
+        <a id="logo" href="" title="Open Science Framework Sign In">Open Science Framework | Sign In</a>
+
+      </header>
+      <div id="content">
+
+  <div id="msg" class="errors">
+
+    <h2>Account has not been confirmed.</h2>
+    <p>This login email has been registered but not confirmed. Please check your email (and spam folder). <a href="https://staging.osf.io/resend/">Click here</a> to resend your confirmation email.</p>
+  </div>
+
+    <div class="row" style="text-align: center;">
+        <hr>
+        <a href="https://staging.osf.io/">Back to OSF</a>
+    </div>
+</div> <!-- END #content -->
+
+<footer>
+    <div class="copyright">
+        <div class="row">
+            <p>Copyright © 2011-2015 <a href="http://centerforopenscience.org">Center for Open Science</a> |
+                <a href="https://github.com/CenterForOpenScience/centerforopenscience.org/blob/master/TERMS_OF_USE.md">Terms of Use</a> |
+                <a href="https://github.com/CenterForOpenScience/centerforopenscience.org/blob/master/PRIVACY_POLICY.md">Privacy Policy</a>
+            </p>
+        </div>
+    </div>
+</footer>
+
+</div> <!-- END #container -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/headjs/1.0.3/head.min.js"></script>
+<script type="text/javascript" src="//staging-accounts.osf.io/js/cas.js"></script>`

90536ac89c9453b6874d67f76ee48bb0117c1283
Sam Chrisinger <s.chrisinger@gmail.com>
2016-04-06 16:34:02 -0400

    Support personal access tokens
    Support personal access tokens

--- types.go
+++ types.go
@@ -10,6 +10,7 @@ type OAuthAttributes struct {
 type OAuthResponse struct {
 	Id         string          `json:"id"`
 	Attributes OAuthAttributes `json:"attributes"`
+	Scope      []string        `json:"scope"`
 }
 
 type User struct {
@@ -23,14 +24,22 @@ type User struct {
 }
 
 type ServiceResponse struct {
-	Xmlns        string   `xml:"xmlns:cas,attr"`
-	XMLName      xml.Name `xml:"cas:serviceResponse"`
-	User         string   `xml:"cas:authenticationSuccess>cas:user"`
-	NewLogin     bool     `xml:"cas:authenticationSuccess>cas:attributes>cas:isFromNewLogin"`
-	Date         string   `xml:"cas:authenticationSuccess>cas:attributes>cas:authenticationDate"`
-	GivenName    string   `xml:"cas:authenticationSuccess>cas:attributes>cas:givenName"`
-	FamilyName   string   `xml:"cas:authenticationSuccess>cas:attributes>cas:familyName"`
-	LongTermAuth bool     `xml:"cas:authenticationSuccess>cas:attributes>cas:longTermAuthenticationRequestTokenUsed"`
-	AccessToken  string   `xml:"cas:authenticationSuccess>cas:attributes>accessToken"`
-	UserName     string   `xml:"cas:authenticationSuccess>cas:attributes>username"`
+	Xmlns            string   `xml:"xmlns:cas,attr"`
+	XMLName          xml.Name `xml:"cas:serviceResponse"`
+	User             string   `xml:"cas:authenticationSuccess>cas:user"`
+	NewLogin         bool     `xml:"cas:authenticationSuccess>cas:attributes>cas:isFromNewLogin"`
+	Date             string   `xml:"cas:authenticationSuccess>cas:attributes>cas:authenticationDate"`
+	GivenName        string   `xml:"cas:authenticationSuccess>cas:attributes>cas:givenName"`
+	FamilyName       string   `xml:"cas:authenticationSuccess>cas:attributes>cas:familyName"`
+	LongTermAuth     bool     `xml:"cas:authenticationSuccess>cas:attributes>cas:longTermAuthenticationRequestTokenUsed"`
+	AccessToken      string   `xml:"cas:authenticationSuccess>cas:attributes>accessToken"`
+	AccessTokenScope string   `xml:"cas:authenticationSuccess>cas:attributes>accessTokenScope"`
+	UserName         string   `xml:"cas:authenticationSuccess>cas:attributes>username"`
+}
+
+type AccessToken struct {
+	Id      string `bson:"_id"`
+	Owner   string `bson:"owner"`
+	TokenId string `bson:"token_id"`
+	Scopes  string `bson:"scopes"`
 }
